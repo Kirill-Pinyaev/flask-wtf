545f9000f2f95294b180f5953e0f5d79f10e5171
Кирилл <pinyaev-kirill@ya.ru>
2020-03-09 17:54:25 +0300

    1 сомит

---
+++ Готовимся к миссии.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import random
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index/<title>')
+def index(title):
+    if 'инженер' in title:
+        return render_template('base.html', title=title, img=1)
+    elif 'строитель' in title:
+        return render_template('base.html', title=title, img=2)
+    else:
+        return render_template('base.html', title=title, img=random.randint(1, 2))
+
+
+if __name__ == '__main__':
+    app.run('', 8080)
